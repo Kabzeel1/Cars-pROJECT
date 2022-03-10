621fa7f1343f400694fe77a2343fc965dc06f396
Kabzeel <ian.heltonn@gmail.com>
2022-03-09 21:30:05 -0500

    profiles

--- models/profile.js
+++ models/profile.js
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose'
 
+onst catSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  breed: String
+}, {
+  timestamps: true,
+})
+
+
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
